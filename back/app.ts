f5978b54b7ba3b44ff4f76e615dba8a239322876
ayzapata <ayzapata@student.42.fr>
2023-04-02 18:49:01 +0200

    set https server

--- back/app.ts
+++ back/app.ts
@@ -2,10 +2,13 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import { authRouters } from "./routers/authRouters";
 import { dataSource } from "./data/app-data-source";
+import * as fs from "fs";
+import * as https from "https";
 
 import "reflect-metadata";
 
 dotenv.config({ path: __dirname + "/.env" });
+const port = process.env.PORT;
 
 // establish database connection
 dataSource
@@ -20,13 +23,16 @@ dataSource
 const app: Express = express();
 app.use(express.json());
 
-const port = process.env.PORT;
+const httpsOpitons = {
+  cert: fs.readFileSync("/etc/node/ssl/server.crt"),
+  key: fs.readFileSync("/etc/node/ssl/server.key"),
+};
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server:)");
 });
+app.use(authRouters);
 
-app.listen(port, () => {
+https.createServer(httpsOpitons, app).listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
-app.use(authRouters);
