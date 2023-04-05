ffd6c4bff54975f8bf660af75f23a324a3263cb1
ayzapata <ayzapata@student.42.fr>
2023-04-05 23:08:35 +0200

    README.md

---
+++ README.md
@@ -0,0 +1,23 @@
+# API REST Justify text
+
+Stack:
+- Typescript
+- Nodejs
+- TypeOrm
+- PostgreSQL
+- JWT
+- Docker compose
+
+## Environment variables
+Please update `.env`
+
+## Running the application
+From the project's root folder run:
+```
+docker compose up --build
+```
+
+- Check API at https://localhost:3000/
+
+## Resquests list
+`Requests.http` file contains the list of requests to the API. (Execute with REST Client extension in VS Code)
--- back/app.ts
+++ back/app.ts
@@ -30,11 +30,11 @@ const httpsOpitons = {
 };
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server:)");
+  res.send("Express + TypeScript Server :)");
 });
 app.use(authRouters);
 app.use(justifyRouters);
 
 https.createServer(httpsOpitons, app).listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 });
